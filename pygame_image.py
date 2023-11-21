4c7de42365ca51727f287df2b3423e5c65549671
c0a2201328 <c0a2201328@edu.teu.ac.jp>
2023-11-21 17:12:45 +0900

    演習

--- pygame_image.py
+++ pygame_image.py
@@ -10,15 +10,24 @@ def main():
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_lis = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]#3
+    bg_img1 = pg.transform.flip(bg_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = (tmr%1600)
+        x = (tmr%3200)
+        if tmr % 50 <= 25:
+            num= 0
+        else:
+            num= 1
+
+        
         screen.blit(bg_img, [-x, 0])#4 6
-        screen.blit(bg_img, [1600-x, 0])#6
-        screen.blit(kk_lis[tmr%2], [300, 200])#5
+        screen.blit(bg_img1, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        #screen.blit(bg_img, [1600-x, 0])#6
+        screen.blit(kk_lis[num], [300, 200])#5
     
         pg.display.update()
         tmr += 1        
